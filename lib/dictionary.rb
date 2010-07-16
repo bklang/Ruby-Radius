bc229fc0e856b195e7b9ffce28204fc088d212d6
Ben Klang <ben@alkaloid.net>
2010-07-16 14:30:16 -0400

    Re-work the dictionary parsing code.
    Add back in helper methods needed by Auth and Packet for sending RADIUS packets.
    Improve FreeRADIUS dictionary parsing code and properly support per-attribute values

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -8,15 +8,17 @@ module Radius
 
   class Dictionary
     def initialize
-      @dictionary={}
-      @dictionary[0]={}
-      @dictionary[0]["ATTRIBUTES"]={}
-      @dictionary[0]["VALUES"]={}
+      @dictionary = {}
     end
 
     def load(dictionary_file)
       # set the default vendor id
       vendor_id=0
+
+      # Temporary holding spot for values being defined that have not yet had
+      # their attributes defined.
+      orphan_values = {}
+
       File.open(dictionary_file, "r").each_line do |line|
 
         next if line =~ /^\#/    # discard comments
@@ -25,56 +27,109 @@ module Radius
         token=tokens[0].upcase
 
         case token
-          when "$INCLUDE"
-            # This is a FreeRADIUS-style file inclusion directive
-            file = tokens[1]
-            if file =~ /^\//
-              # File names with a leading slash are absolute
-              load(tokens[1])
-            else
-              # File names without a leading slash are relative
-              # to the current file
-              load(File.dirname(dictionary_file) + "/" + file)
-            end
+        when "$INCLUDE"
+          #Example lines
+          #$INCLUDE        /usr/share/freeradius/dictionary
 
-          when "VENDOR"    # Setup a new hash to hold attributes and values for the vendor id
-            #Example line
-            #VENDOR    Digium        22736
+          # This is a FreeRADIUS-style file inclusion directive
+          linetype, file = tokens
+          file.strip!
 
-            #I am assuming that all attributes and values will belong to the last
-            #"declared" vendor, until a new vendor is encountered
-            vendor_id=tokens[2].to_i
-            if @dictionary[vendor_id].nil?
-              @dictionary[vendor_id]={}
-              @dictionary[vendor_id]["ATTRIBUTES"]={}
-              @dictionary[vendor_id]["VALUES"]={}
-              @dictionary[vendor_id]["NAME"]=tokens[1].strip
-            end
-          when "ATTRIBUTE"
-            #Example lines
-            #ATTRIBUTE       Asterisk-Acc-Code               101     string  Digium
-            #ATTRIBUTE	   Acct-Delay-Time		           41	   integer
-            @dictionary[vendor_id][tokens[2].to_i]={}
-            @dictionary[vendor_id][tokens[2].to_i]["NAME"]=tokens[1].strip
-            @dictionary[vendor_id][tokens[2].to_i]["TYPE"]=tokens[3].strip
-            #also make a hash key of the attribute name b/c we need it to store values
-            @dictionary[vendor_id][tokens[1].strip]={"ID"=>tokens[2].to_i, "TYPE"=>tokens[3].strip}
-#debug            puts "#{vendor_id}[#{tokens[2].to_i}][\"TYPE\"]=>#{tokens[3]}"
-          when "VALUE"
-            #Example line
-            #VALUE		Service-Type	Callback-Login-User	3
-            #Store a name key, and an id key
-            @dictionary[vendor_id][tokens[1]]={tokens[2].strip=>tokens[3].to_i}
-            @dictionary[vendor_id][tokens[1]]={tokens[3].to_i=>tokens[2].strip}
+          # Load the referenced file by calling this function recursively
+          if file =~ /^\//
+            # File names with a leading slash are absolute
+            load(tokens[1])
+          else
+            # File names without a leading slash are relative
+            # to the current file
+            load(File.dirname(dictionary_file) + "/" + file)
+          end
+
+        when "VENDOR"
+          #Example line
+          #VENDOR    Digium        22736
+
+          # Setup a new hash to hold attributes and values for the vendor id
+          linetype, name, id = tokens
+          name.strip!
+
+          if @dictionary[id].nil?
+            @dictionary[id] = { :name => name }
+            @dictionary[name] = id
+          end
+
+        when "BEGIN-VENDOR"
+          #Example lines
+          #BEGIN-VENDOR Digium
+
+          # We have a new active vendor ID for the following attributes
+          linetype, name = tokens
+          name.strip!
+          vendor_id = @dictionary[name]
+          raise "Parse error: unknown vendor" if vendor_id.nil?
+
+        when "END-VENDOR"
+          #Example lines
+          #END-VENDOR Digium
+
+          # We've reached the end of this vendor's definitions.  Reset to the
+          # no-vendor vendor ID.
+          vendor_id = 0
+
+        when "ATTRIBUTE"
+          #Example lines
+          #ATTRIBUTE       Asterisk-Acc-Code               101     string  Digium
+          #ATTRIBUTE	   Acct-Delay-Time		           41	   integer
+          linetype, name, number, type = tokens
+          name.strip!
+          number = number.to_i
+          type.strip!
+          type.downcase!
+          vendor = get_vendor_name_by_id(vendor_id)
+
+          @dictionary[vendor_id][number] = {}
+          @dictionary[vendor_id][number][:name] = name
+          @dictionary[vendor_id][number][:type] = name
+          @dictionary[vendor_id][name] = number
+
+          if orphan_values.key_key(number)
+            @dictionary[vendor_id][number][:values] = orphan_values.delete(number)
+          end
+
+        when "VALUE"
+          #Example line
+          #VALUE		Service-Type	Callback-Login-User	3
+
+          # These are enumerables.  The name of the value matches the name of
+          # an attribute, and (only?) the defined values are legal
+
+          #Store a name key, and an id key
+          linetype, attr, value_name, value_id = tokens
+          attr = type.strip!
+          value_name.strip!
+          value_id = value_id.to_i
+          begin
+            attr = attr_num(attr, vendor_id)
+            @dictionary[vendor_id][attr][:values] = {
+              value_id => value_name,
+              value_name => value_id
+            }
+          rescue
+            # The attribute must not have yet been defined.  Stash it for now
+            # and add it when we eventually parse the attribute.
+            orphan_values[attr][value_id] = value_name
+            orphan_values[attr][value_name] = value_id
+          end
         end
       end
-#      puts @dictionary.inspect
+
+      raise "Orphan values detected" if orphan_values.length
 
     end
 
     def get_attribute_type_by_id(vendor_id, attribute_id)
        if @dictionary[vendor_id]!=nil &&  @dictionary[vendor_id][attribute_id]!=nil
-         @dictionary[vendor_id][attribute_id]["TYPE"]
+         @dictionary[vendor_id][attribute_id][:type]
        else
          return "string"
        end
@@ -82,18 +137,83 @@ module Radius
 
     def get_attribute_name_by_id(vendor_id,attribute_id)
       if @dictionary[vendor_id]!=nil &&  @dictionary[vendor_id][attribute_id]!=nil
-        @dictionary[vendor_id][attribute_id]["NAME"]
+        @dictionary[vendor_id][attribute_id][:name]
       else
-        return "unknown attribute"
+        raise "unknown attribute"
       end
     end
 
     def get_vendor_name_by_id(vendor_id)
       if @dictionary[vendor_id]!=nil
-        @dictionary[vendor_id]["NAME"]
+        @dictionary[vendor_id][:name]
       else
-        return "unknown vendor"
+        raise "unknown vendor"
       end
+    end
+
+    def vendor_id(name)
+      @dictionary[name]
+    end
+
+    def attr_num(attr, vendor = 0)
+      if vendor.class == "String"
+        # Look up the vendor ID by name
+        vendor = @dictionary[vendor] or raise "unknown vendor"
+      end
+
+      raise "unknown attribute" if @dictionary[vendor][attr].nil?
+      @dictionary[vendor][attr]
+    end
+
+    def attr_type(attr, vendor = 0)
+      if vendor.class == "String"
+        # Look up the vendor ID by name
+        vendor = @dictionary[vendor] or raise "unknown vendor"
+      end
+
+      if attr.class == "String"
+        # Look up the attribute ID by name
+        attr = attr_num(attr, vendor)
+      end
+
+      @dictionary[vendor][attr][:type]
+    end
+
+    def attr_has_val?(attr)
+      vsattr_has_val(attr, 0)
+    end
+
+    def vsattr_has_val(attr, vendor = 0)
+      if vendor.class == "String"
+        # Look up the vendor ID by name
+        vendor = @dictionary[vendor] or raise "unknown vendor"
+      end
+
+      if attr.class == "String"
+        # Look up the attribute ID by name
+        attr = attr_num(attr, vendor)
+      end
+
+      @dictionary[vendor][attr].has_key?(:values)
+    end
+
+    def val_num(attr, code)
+      vsaval_num(attr, 0, code)
+    end
+
+    def vsaval_num(attr, vendor, code)
+      if vendor.class == "String"
+        # Look up the vendor ID by name
+        vendor = @dictionary[vendor] or raise "unknown vendor"
+      end
+
+      if attr.class == "String"
+        # Look up the attribute ID by name
+        attr = attr_num(attr, vendor)
+      end
+
+      raise "Unknown code" if @dictionary[vendor][attr][:values][code].nil?
+      @dictionary[vendor][attr][:values][code]
     end
 
   end
